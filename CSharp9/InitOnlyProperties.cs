055b2cb2e2adb50584fe84866f949d70c073fa95
Houssam Rouidi <houssameddine.rouidi-ext@vinci-autoroutes.com>
2020-07-26 16:41:35 +0200

    Add some CSharp 9 feartures

---
+++ CSharp9/InitOnlyProperties.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp9
+{
+    public class InitOnlyProperties
+    {
+        //public class Doto(string name);
+        public class Person
+        {
+            private readonly string _firstName;
+            private readonly string _lastName;
+
+            public string FirstName
+            {
+                get => _firstName;
+                init => _firstName = value ?? throw new ArgumentNullException(nameof(FirstName));
+            }
+            public string LastName
+            {
+                get => _lastName;
+                init => _lastName = value ?? throw new ArgumentNullException(nameof(LastName));
+            }
+        }
+        
+        //public data class Personz(string FirstName, string LastName)
+        [Test]
+        public static void InitOnlyProperties_Test()
+        {
+            var person = new Person { FirstName = "Houssam", LastName = "Rouidi" };
+            Assert.AreEqual(("Houssam", "Rouidi"), (person.FirstName, person.LastName));
+            //person.LastName = "notAllowed"; // Does'nt complie
+        }
+    }
+}
---
+++ CSharp9/RecordType.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CSharp9
+{
+    public class RecordType
+    {
+        public record Person(string FirtName, string LastName);
+
+        [Test]
+        public static void RecordType_Test()
+        {
+            var me = new Person("Houssam", "Rouidi");
+            Assert.AreEqual(("Houssam", "Rouidi"), (me.FirtName, me.LastName));
+            var alterEgo = new Person("Houssam", "Rouidi");
+            // reference equality
+            Assert.AreNotSame(me, alterEgo);
+            Assert.IsFalse(me== alterEgo);
+            //structured equality
+            Assert.IsTrue(me.Equals(alterEgo));
+            Assert.AreEqual(me.GetHashCode(), alterEgo.GetHashCode());
+            var (f, l) = me;
+            Assert.AreEqual("Houssam", f);
+            Assert.AreEqual("Rouidi", l);
+            //me.FirtName = "kk"; Does'not compile
+            var you = me with{FirtName="You"}
+            ;
+            Assert.AreEqual("You", you.FirtName);
+            Assert.AreEqual("Rouidi", you.LastName);
+        }
+    }
+}
